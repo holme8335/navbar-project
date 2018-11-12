530f7d6f4a7eee83730e8f2698fcbe2f0d4333f3
holme8335 <44613050+holme8335@users.noreply.github.com>
2018-11-12 16:58:34 -0500

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,14 @@ function profileClick() {
 document.getElementById('header-email-div').onclick = function() {
     var arrow = document.getElementById('email-arrow');
     var showDiv = document.getElementById('email-container');
+    var border = document.getElementById('header-email-span');
         arrow.classList.toggle("js-rotated-180");
         showDiv.classList.toggle("hide");
+        if (border.classList.contains("selector-border") == false) {
+            border.classList.add("selector-border");
+        } else {
+            border.classList.remove("selector-border");
+        };
 };
 
 document.getElementById('sub-menu').onclick = function() {
